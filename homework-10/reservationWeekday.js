afda538d41513a0901101a25990d91e50224ab90
antonio.hadrovic <hadrovic1@gmail.com>
2019-03-27 23:04:49 +0100

    Import luxon, array with reserved halls returns empty - incomplete

--- homework-10/reservationWeekday.js
+++ homework-10/reservationWeekday.js
@@ -1,3 +1,33 @@
+//import halls array
+
+var halls = require ('./halls.js');
+
+
+//import luxon
+const { DateTime } = require("luxon");
+
+//an array of reserved halls - returns empty array
+
+function reserved(input, field) {
+    var output = [];
+    for (var i =0; i < input.length; ++i)
+        if  (field = true) {
+            output.push(input[i][field]);
+            
+        }
+        return output;
+    }
+
+
+
+
+
+var result = reserved(halls, "reservation.isReserved");
+
+
+var hallsToString = result.join(", ");
+
+
 
 // server
 
@@ -6,7 +36,7 @@ var http = require("http");
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    response.end("Hello World");
+    response.end(hallsToString);
 }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081/');
