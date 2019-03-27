256efdf8cd439a39701ab4da6cd8a546dd2431c9
antonio.hadrovic <hadrovic1@gmail.com>
2019-03-27 23:02:46 +0100

    Add halls with unique name via lodash

--- homework-10/uniqHalls.js
+++ homework-10/uniqHalls.js
@@ -1,3 +1,30 @@
+//import halls array
+
+var halls = require ('./halls.js');
+
+//import Lodash
+
+var _ = require('lodash');
+
+// array of halls with unique names
+
+
+function getFields(input, field) {
+    var output = [];
+    for (var i=0; i < input.length ; ++i)
+        output.push(input[i][field]);
+    return output;
+}
+
+var result = getFields(halls, "name");
+
+//creating new array with unique hall names
+
+var noDuplicates = _.uniq(result);
+
+var hallsToString = noDuplicates.join(", ");
+
+
 
 // server
 
@@ -6,7 +33,7 @@ var http = require("http");
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    response.end("Hello World");
+    response.end(hallsToString);
 }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081/');
